ae91d661115fd1c78064d3b12979efbf9c55d841
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-09-05 14:04:32 +0530

    chore: Add Tailwind CSS configuration and Vite configuration files

--- backend/src/controllers/Pyq.controller.js
+++ backend/src/controllers/Pyq.controller.js
@@ -2,3 +2,4 @@ import { ApiErrors } from "../utils/ApiErrors";
 import { asyncHandler } from "../utils/asyncHandler";
 import { Pyq } from "../models/Pyq.model";
 import { ApiResponse } from "../utils/ApiResponseHandler";
+
