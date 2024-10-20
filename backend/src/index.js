5dabde6de485e49d4d10e23b531a34024677b274
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-10-21 02:07:51 +0530

    Update server port to use dynamic value from environment variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,8 +17,8 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
-        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    app.listen(process.env.PORT, "0.0.0.0", () => {
+        console.log(`Server is running on port ${process.env.PORT || 8080}`);
     })
 })
 .catch ((error) => {
